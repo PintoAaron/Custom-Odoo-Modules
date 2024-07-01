0d51777e379aa37ac2103f9ac6e610006239895b
apinto <apinto@quantumgroupgh.com>
2024-07-01 13:50:46 +0000

    add library module

---
+++ library/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Library',
+    'version': '14.0.0.0',
+    'application': True,
+    'auto_install': False,
+    'installable': True,
+    'summary': 'Your all books last stop',
+    'description': 'reading makes you brilliant',
+    'license': 'LGPL-3',
+    'sequence': 15,
+    'author': 'Pinto T. Aaron',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        'views/library_user.xml',
+    ],
+    
+}
